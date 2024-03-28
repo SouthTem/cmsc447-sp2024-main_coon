8ec5022046baeee060b1e0027d93ee8713257e85
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2024-03-28 14:12:29 -0400

    login, run, coins functions added
    Cleaned up and added functions for going from javascript to the server and back.

--- Sprint2/Game/app.py
+++ Sprint2/Game/app.py
@@ -42,10 +42,31 @@ def login():
             token = create_access_token(identity=account.id)
             print("token:", token)
             
-            return jsonify(access_token=token)
+            return jsonify({'access_token': token})
     except Exception as e:
         print(e)
-        return "AHH"
+        return jsonify({'success': False, 'message': str(e)}), 400
+    
+@app.route('/run', methods=['POST'])
+@jwt_required()
+def add_run():
+    try:
+        data:dict = request.json
+        points = data.get('points', 0)
+        coins = data.get('coins', 0)
+        level_id = data.get('level_id', 1)
+
+        current_account_id = get_jwt_identity()
+        player:database.Player = database.Player.query.filter_by(id=current_account_id).one_or_none()
+
+        if player is not None:
+            new_run:database.Run = database.Run(points=points, coins=coins, level_id=level_id)
+            player.runs.append(new_run)
+            database.db.session.commit()
+            return jsonify({'success': True}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'success': False, 'message': str(e)}), 400
     
 @app.route('/update_coins', methods=['POST'])
 @jwt_required()
@@ -59,12 +80,16 @@ def update_coins():
         player:database.Player = database.Player.query.filter_by(id=current_account_id).one_or_none()
 
         if player is not None:
+            new_coins = player.coins + coins
+            if new_coins < 0:
+                raise ValueError('coin count cannot be negative')
+            
             player.coins += coins
             database.db.session.commit()
             return jsonify({'success': True})
     except Exception as e:
         print(e)
-        return jsonify({'success': False})
+        return jsonify({'success': False, 'message':str(e)}), 400
 
 @app.route('/')
 def index():
--- Sprint2/Game/static/flask_functions.js
+++ Sprint2/Game/static/flask_functions.js
@@ -1,12 +1,28 @@
-function login()
+const LOGIN_ENDPOINT = "/login";
+const RUN_ENDPOINT = "/run";
+const COIN_ENDPOINT = "/update_coins"
+
+function get_token()
+{
+    return sessionStorage.getItem('access_token');
+}
+
+/**
+ * Sends a request to the /login endpoint in order to
+ * create an access token in session storage which is required
+ * for interactions with most flask endpoints
+ * @param {string} username - the user's username
+ * @param {string} password - the user's password
+ */
+function login(username, password)
 {
     var data = JSON.stringify
     ({
-        username: "test",
-        password: "pass"
+        username: username,
+        password: password
     });
 
-    fetch("/login", {
+    fetch(LOGIN_ENDPOINT, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -14,37 +30,45 @@ function login()
         body: data
     })
     .then(response => {
-        if (response.ok)
-        {
-            response.json().then(data =>{
-                console.log("Login Success")
-                console.log("data:", data)
-    
-                // add the token to session storage (reloading the page will remove it)
-                // alternatively could be added to local storage which lasts for longer
-                sessionStorage.setItem("access_token", data.access_token)
-            });
-        }
-        else 
-        {
-            console.error("Error", response.statusText);
-        }
+        if (!response.headers.get("content-type")?.includes("application/json"))
+            throw new Error(`Expected a json response from ${LOGIN_ENDPOINT}`);
+
+        if (!response.ok) 
+            throw new Error(`Response from ${LOGIN_ENDPOINT} was no ok`);
+        
+        return response.json();
+    })
+    .then(json => {
+        console.log('login', json);
+
+        // add the token to session storage (reloading the page will remove it)
+        // alternatively could be added to local storage which lasts for longer
+        sessionStorage.setItem("access_token", json.access_token);
     })
     .catch(error => {
         console.error("Error:", error);
     });
 }
 
-function addCoins()
+/**
+ * Sends a request to the /run endpoint in order to
+ * update the database with a new run
+ * @param {integer} points number of points earned in the run
+ * @param {integer} coins number of coins obtained in the run
+ * @param {integer} levelId id of the level just ran
+ */
+function addRun(points, coins, levelId)
 {
     var data = JSON.stringify
     ({
-        coins: 999
+        points: points,
+        coins: coins,
+        level_id: levelId 
     });
 
-    const token = sessionStorage.getItem('access_token')
+    token = get_token();
 
-    fetch("/update_coins", {
+    fetch(RUN_ENDPOINT, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -53,18 +77,61 @@ function addCoins()
         body: data
     })
     .then(response => {
-        if (response.ok)
+        if (!response.headers.get("content-type")?.includes("application/json"))
+            throw new Error(`Expected a json response from ${RUN_ENDPOINT}`);
+
+        if (!response.ok)
         {
-            response.json().then(data =>{
-                console.log("data:", data)
-            });
+            console.warn(response.json())
+            throw new Error(`Response from ${RUN_ENDPOINT} was no ok`);
         }
-        else 
-        {
-            console.error("Error", response.statusText);
-        }
+
+        
+        return response.json();
+    })
+    .then(json => {
+        console.log('addRun', json);
     })
     .catch(error => {
-        console.error("Error:", error);
+        console.error(error);
+    });
+}
+
+/**
+ * Sends a request to the /update_coins endpoint in order to
+ * update the database with a new coin count for the player
+ * @param {integer} coins how many to coins (+ or -) to add to the players total
+ */
+function addCoins(coins)
+{
+    var data = JSON.stringify
+    ({
+        coins: coins
+    });
+
+    const token = get_token()
+
+    fetch(COIN_ENDPOINT, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': 'Bearer ' + token
+        },
+        body: data
+    })
+    .then(response => {
+        if (!response.headers.get("content-type")?.includes("application/json"))
+            throw new Error(`Expected a json response from ${COIN_ENDPOINT}`);
+
+        if (!response.ok) 
+            throw new Error(`Response from ${COIN_ENDPOINT} was no ok`);
+        
+        return response.json();
+    })
+    .then(json => {
+        console.log('addCoins', json);
+    })
+    .catch(error => {
+        console.log(error);
     });
 }
