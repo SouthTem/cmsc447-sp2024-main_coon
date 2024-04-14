98fc571d9f4be0738e8fb331076219583e2aef37
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2024-04-13 21:19:47 -0400

    fix texture gaps

--- Sprint2/Game/static/scenes/Game.js
+++ Sprint2/Game/static/scenes/Game.js
@@ -2,10 +2,7 @@
 
 var player;
 
-var platforms;
-var obstacles;
 var coins;
-
 var floors;
 var ceilings;
 
@@ -20,7 +17,7 @@ var cursors;
 var gravityKey;
 var gravity;
 var isFlipped = false;
-var scrollSpeed = 100;
+var scrollSpeed = 24 * 5;
 var isGameOver = false;
 let topY = 0;
 let bottomY = 500;
@@ -36,6 +33,8 @@ var spawnTileY = Math.ceil(maxTileHeight / 2);
 var floorTileY = maxTileHeight;
 var ceilingTileY = 0;
 
+var floorHeight = 0;
+var ceilingHeight = 0;
 
 
 class Game extends Phaser.Scene
@@ -44,6 +43,9 @@ class Game extends Phaser.Scene
     {
         super('Game');
     }
+
+    lastCeiling;
+    lastGround;
 
     createCoin(x, y)
     {
@@ -64,51 +66,18 @@ class Game extends Phaser.Scene
         scoreText.setText('Score: ' + score);
     }
 
-    createHorizontalPlatform(x, y, width, sprite = 'wooden')
-    {
-        var platform = platforms.create(x, y, sprite);
-        platform.setOrigin(0, 0);
-        platform.setVelocityX(-scrollSpeed);
-        platform.displayWidth = width;
-        platform.setDepth(gameDepth);
-        return platform;
-    }
-
-    // clean this up and define a grid so that tiles look good
-    createVerticalPlatform(x = 0, y = 0, widthMin = 25, widthMax = 50, heightMin = 100, heightMax = 200)
-    {
-        var height = Phaser.Math.Between(heightMin, heightMax);
-        var width = Phaser.Math.Between(widthMin, widthMax);
-        //var obstacle = obstacles.create(x, y, 'wooden').setVelocityX(-scrollSpeed);
-        var obstacle = this.add.tileSprite(x, y, width, height, 'wooden').setScale(2);
-        obstacles.add(obstacle);
-        this.physics.add.existing(obstacle, false);
-
-        //block.body.setVelocity(-scrollSpeed, 0);
-        obstacle.body.setVelocityX(-scrollSpeed);
-        obstacle.setOrigin(0, 0);
-        obstacle.displayWidth = width;
-        obstacle.displayHeight = height;
-        obstacle.setDepth(gameDepth);
-        return obstacle;
-    }
-
-    createRandomObstacle(sprite = 'wooden', group, gridX = 0, gridY = 0, widthMin = 1, widthMax = 3, heightMin = 1, heightMax = 4)
+    createRandomTile(sprite = 'wooden', group, x = 0, y = 0, widthMin = 1, widthMax = 3, heightMin = 1, heightMax = 4)
     {
         var height = Phaser.Math.Between(heightMin, heightMax);
         var width = Phaser.Math.Between(widthMin, widthMax);
 
-        return this.createTile(gridX, gridY, width, height, sprite, group);
+        return this.createTile(x, y, width, height, sprite, group);
     }
 
-    createTile(gridX = 0, gridY = 0, width = 1, height = 1, sprite = 'wooden', group)
+    createTile(x = 0, y = 0, width = 1, height = 1, sprite = 'wooden', group)
     {
-        let x = gridX * gridSize;
-        let y = gridY * gridSize;
         let realWidth = width * gridSize;
         let realHeight = height * gridSize;
-
-        console.log(x, y, width, height)
 
         var obstacle = this.add.tileSprite(x, y, realWidth, realHeight, sprite).setScale(tileScale);
         group.add(obstacle);
@@ -127,16 +96,6 @@ class Game extends Phaser.Scene
         this.add.image(400, 300, 'sky');
         gravity = config.physics.arcade.gravity.y;
 
-        platforms = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
-
-        obstacles = this.physics.add.group({
-            allowGravity: false,
-            immovable: true
-        });
-
         ceilings = this.physics.add.group({
             allowGravity: false,
             immovable: true
@@ -152,11 +111,6 @@ class Game extends Phaser.Scene
             immovable: true
         });
 
-        console.log(coins);
-
-        //this.createHorizontalPlatform(0, 0, 1200);
-        //this.createHorizontalPlatform(0, 500, 1200);
-
         player = this.physics.add.sprite(spawnTileX * gridSize, spawnTileY * gridSize, 'dog').setScale(2);
 
         this.anims.create({
@@ -171,8 +125,6 @@ class Game extends Phaser.Scene
         //player.setBounce(0.2);
         //player.setCollideWorldBounds(true);
 
-        this.physics.add.collider(player, platforms);
-        this.physics.add.collider(player, obstacles);
         this.physics.add.collider(player, ceilings);
         this.physics.add.collider(player, floors);
 
@@ -187,15 +139,9 @@ class Game extends Phaser.Scene
 
         for(var i = 0; i < maxTileWidth; ++i)
         {
-            this.createGround('wooden', i);
-            this.createCeiling('wooden', i);
+            this.createGround('wooden', i * gridSize);
+            this.createCeiling('wooden', i * gridSize);
         }
-
-        //this.createVerticalPlatforms(true, Phaser.Math.Between(0, 100));
-
-        //this.createVerticalPlatforms(false, 300 + Phaser.Math.Between(0, 100));
-
-        //scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
         scoreText = this.add.text(16, 16, 'Score: 0', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
@@ -204,32 +150,21 @@ class Game extends Phaser.Scene
         scoreText.setDepth(textDepth);
     }
 
-    createGround(sprite = 'wooden', gridX = 0)
+    createBetterGround(sprite = 'wooden')
     {
-        this.createRandomObstacle(sprite, floors, gridX, floorTileY, 1, 1, 1, 10).setOrigin(0, 1);
+
     }
 
-    createCeiling(sprite = 'wooden', gridX = 0)
+    
+
+    createGround(sprite = 'wooden', x = 0)
     {
-        this.createRandomObstacle(sprite, ceilings, gridX, ceilingTileY, 1, 1, 1, 8);
+        this.lastGround = this.createRandomTile(sprite, floors, x, floorTileY * gridSize, 1, 1, 1, 10).setOrigin(0, 1);
     }
 
-    createVerticalPlatforms(top=true, startX=0)
+    createCeiling(sprite = 'wooden', x = 0)
     {
-        let maxWidth = config.width;
-        let maxHeight = config.height;
-        
-
-        let currWidth = startX;
-        while (currWidth < maxWidth)
-        {
-            var platform;
-            if (top) 
-                platform = this.createVerticalPlatform(currWidth, topY, 50, 200, 100, 200)//.refreshBody();
-            else
-                platform = this.createVerticalPlatform(currWidth, bottomY, 50, 200, 50, 100).setOrigin(0,1);
-            currWidth += platform.displayWidth + Phaser.Math.Between(-50, 50);
-        }
+        this.lastCeiling = this.createRandomTile(sprite, ceilings, x, ceilingTileY * gridSize, 1, 1, 1, 8);
     }
 
     update()
@@ -284,15 +219,6 @@ class Game extends Phaser.Scene
             this.createCoin(coinX, coinY);
         }
 
-        // move the ceiling and floor platforms to keep them on scren
-        platforms.children.iterate(child =>
-        {
-            if (child.x <= -child.width)
-            {
-                child.x += child.width;
-            }
-        });
-
         // remove all the vertical walls that have gone offscreen
         let deleteCount = 0;
         let posX = 0;
@@ -302,7 +228,7 @@ class Game extends Phaser.Scene
             let child = floors.children.entries[i];
             if (child.x <= -child.displayWidth)
             {
-                posX = Math.ceil(child.x / gridSize);
+                posX = child.x;
                 deleteCount++;
                 floors.children.entries.splice(i, 1);
                 i--;
@@ -312,7 +238,7 @@ class Game extends Phaser.Scene
         // add in new vertical walls for each wall that was deleted
         for (var i = 0; i < deleteCount; ++i)
         {
-            this.createGround('wooden', posX + maxTileWidth);
+            this.createGround('wooden', this.lastGround.body.x + this.lastGround.displayWidth);
         }
 
         deleteCount = 0;
@@ -331,7 +257,8 @@ class Game extends Phaser.Scene
         // add in new vertical walls for each wall that was deleted
         for (var i = 0; i < deleteCount; ++i)
         {
-            this.createCeiling('wooden', posX + maxTileWidth);
+            console.log(posX);
+            this.createCeiling('wooden', this.lastCeiling.body.x + this.lastCeiling.displayWidth);
         }
 
         // delete coins that go offscreen
